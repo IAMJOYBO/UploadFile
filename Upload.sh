8f221dac727a0b43fb745f856a1cf0a527f418c0
IAMJOYBO <160575243+IAMJOYBO@users.noreply.github.com>
2025-04-15 22:32:09 +0800

    Create Upload.sh

---
+++ Upload.sh
@@ -0,0 +1,8 @@
+git config --global user.name "IAMJOYBO"
+git config --global user.email "13129311531@163.com"
+git status
+git lfs install
+git lfs track .
+git add .gitattributes .
+git commit -m "Upload Large File"
+git push origin main
